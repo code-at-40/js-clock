398379b5bce79b6053894b7f11c6ce79cebf8711
Alessandro Corra <alessandro.corra@lovehoney.co.uk>
2021-04-18 16:16:44 +0100

    js logic added, small scss refactor

--- js/index.js
+++ js/index.js
@@ -1,1 +1,65 @@
-console.log('hello world!');
+const clock = document.querySelector('.container__clock-circle');
+const secondsLine = document.querySelector('.container__clock-seconds');
+const minutesLine = document.querySelector('.container__clock-minutes');
+const hoursLine = document.querySelector('.container__clock-hours');
+
+const clockRadius = clock.r.baseVal.value;
+const clockCenterX = clock.cx.baseVal.value;
+const clockCenterY = clock.cy.baseVal.value;
+
+const secondsLineLenght = clockRadius - 20;
+const minutesLineLenght = clockRadius - 20;
+const hoursLineLenght = clockRadius - 100;
+
+const getTime = () => {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  return {
+    seconds: seconds,
+    minutes: minutes,
+    hours: hours
+  };
+};
+
+const degreeToRadians = (degree) => {
+  const radians = degree * (Math.PI / 180);
+
+  return radians;
+};
+
+const moveLine = (lineTime, line, lineLength) => {
+  const angleDegreesSecondsMinutes = 360 / 60;
+  const angleDegreesHours = 360 / 12;
+  let nowAngle = 0;
+
+  if (line.id === 'hours') {
+    nowAngle = lineTime * angleDegreesHours;
+  } else {
+    nowAngle = lineTime * angleDegreesSecondsMinutes;
+  }
+
+  const radians = degreeToRadians(90 - nowAngle);
+
+  const newX1 = clockCenterX + Math.round(lineLength * Math.cos(radians));
+  const newY1 = clockCenterY - lineLength + Math.round(lineLength - lineLength * Math.sin(radians));
+
+  line.x1.baseVal.value = newX1;
+  line.y1.baseVal.value = newY1;
+};
+
+const moveLines = () => {
+  const time = getTime();
+
+  moveLine(time.seconds, secondsLine, secondsLineLenght);
+  moveLine(time.minutes, minutesLine, minutesLineLenght);
+  moveLine(time.hours, hoursLine, hoursLineLenght);
+};
+
+moveLines();
+
+setInterval(() => {
+  moveLines();
+}, 1000);
